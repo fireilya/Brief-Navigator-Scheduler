55715cf4aa007a55b600fd87fb769c67339eea36
kk7k3r <119610131+kk7k3r@users.noreply.github.com>
2025-12-13 22:48:04 +0500

    MenuBase

---
+++ Assets/Scripts/Scheduler/Data/LocationData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Scheduler
+{
+    public class LocationData : MonoBehaviour
+    {
+        public Location Location;
+        public bool IsSelected => Location != Location.None;
+        public string Name { get
+            {
+                return Location.ToString();
+            }
+        }
+
+        private void ResetLocation() => Location = Location.None;
+
+        void Start()
+        {
+            ResetLocation();
+        }
+    }
+}
