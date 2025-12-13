95a4d37d2a291ddd90f0031889dc5687849b2be0
kk7k3r <119610131+kk7k3r@users.noreply.github.com>
2025-12-13 23:35:23 +0500

    map menu

--- Assets/Scripts/Scheduler/Data/LocationData.cs
+++ Assets/Scripts/Scheduler/Data/LocationData.cs
@@ -5,18 +5,18 @@ namespace Assets.Scripts.Scheduler
 {
     public class LocationData : MonoBehaviour
     {
-        public Location Location;
-        public bool IsSelected => Location != Location.None;
-        public string Name => Location switch
+        public Location location;
+        public bool IsSelected => location != Location.None;
+        public string Name => location switch
         {
             Location.Garden => "Огород",
             Location.Field => "Поле",
             Location.Greenhouses => "Теплицы",
             Location.None => "Не выбрано",
-            _ => $"Неизвестная локация {(int)Location}" 
+            _ => $"Неизвестная локация {(int)location}" 
         };
 
-        private void ResetLocation() => Location = Location.None;
+        private void ResetLocation() => location = Location.None;
         
         void Start()
         {
