bcca7ba5f1baf1fe840806188db7d37da8f0126f
kk7k3r <119610131+kk7k3r@users.noreply.github.com>
2025-12-13 23:05:21 +0500

    Location data

--- Assets/Scripts/Scheduler/Data/LocationData.cs
+++ Assets/Scripts/Scheduler/Data/LocationData.cs
@@ -7,14 +7,17 @@ namespace Assets.Scripts.Scheduler
     {
         public Location Location;
         public bool IsSelected => Location != Location.None;
-        public string Name { get
-            {
-                return Location.ToString();
-            }
-        }
+        public string Name => Location switch
+        {
+            Location.Garden => "Огород",
+            Location.Field => "Поле",
+            Location.Greenhouses => "Теплицы",
+            Location.None => "Не выбрано",
+            _ => $"Неизвестная локация {(int)Location}" 
+        };
 
         private void ResetLocation() => Location = Location.None;
-
+        
         void Start()
         {
             ResetLocation();
